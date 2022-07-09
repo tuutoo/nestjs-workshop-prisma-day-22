f70a3a5805952daab744b275b482690da8fa7a03
tuutoo.iMac <tuutoo@iMac.com>
2022-07-09 14:36:54 +0800

    feat: add CRUD for articles endpoint

---
+++ src/articles/articles.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+
+@Controller('articles')
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Post()
+  create(@Body() createArticleDto: CreateArticleDto) {
+    return this.articlesService.create(createArticleDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get('drafts')
+  findAllDrafts() {
+    return this.articlesService.findAllDrafts();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const article = await this.articlesService.findOne(+id);
+
+    if (!article) {
+      throw new NotFoundException(`Article with id ${id} not found`);
+    }
+
+    return article;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
+    return this.articlesService.update(+id, updateArticleDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.articlesService.remove(+id);
+  }
+}
---
+++ src/articles/articles.service.ts
@@ -0,0 +1,45 @@
+import { PrismaService } from './../prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private prisma: PrismaService) {}
+  create(createArticleDto: CreateArticleDto) {
+    return this.prisma.article.create({
+      data: createArticleDto,
+    });
+  }
+
+  findAll() {
+    return this.prisma.article.findMany({
+      where: { published: true },
+    });
+  }
+
+  findAllDrafts() {
+    return this.prisma.article.findMany({
+      where: { published: false },
+    });
+  }
+
+  async findOne(id: number) {
+    return this.prisma.article.findUnique({
+      where: { id },
+    });
+  }
+
+  update(id: number, updateArticleDto: UpdateArticleDto) {
+    return this.prisma.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.article.delete({
+      where: { id },
+    });
+  }
+}
