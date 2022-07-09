5c238282bf522481eab747b83b76c2bc71ff526f
tuutoo.iMac <tuutoo@iMac.com>
2022-07-09 14:50:46 +0800

    feat: add decorators for swagger api

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -11,27 +11,34 @@ import {
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { ArticleEntity } from './entities/article.entity';
 
 @Controller('articles')
+@ApiTags('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: ArticleEntity })
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articlesService.create(createArticleDto);
   }
 
   @Get()
+  @ApiCreatedResponse({ type: ArticleEntity, isArray: true })
   findAll() {
     return this.articlesService.findAll();
   }
 
   @Get('drafts')
+  @ApiCreatedResponse({ type: ArticleEntity, isArray: true })
   findAllDrafts() {
     return this.articlesService.findAllDrafts();
   }
 
   @Get(':id')
+  @ApiCreatedResponse({ type: ArticleEntity })
   async findOne(@Param('id') id: string) {
     const article = await this.articlesService.findOne(+id);
 
@@ -43,11 +50,13 @@ export class ArticlesController {
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ type: ArticleEntity })
   update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articlesService.update(+id, updateArticleDto);
   }
 
   @Delete(':id')
+  @ApiCreatedResponse({ type: ArticleEntity })
   remove(@Param('id') id: string) {
     return this.articlesService.remove(+id);
   }
